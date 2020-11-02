a992ebead9125b6d128ddc7f1fa293a43b734264
Paulo <rick.souza97@gmail.com>
2020-11-02 14:09:28 -0300

    Second Commit

---
+++ src/test/java/br/com/xlink/xlink/XlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xlink.xlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
